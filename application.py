1df7e5fa0cea9087aa55221d36f1dadb76d3e1ef
Geoff Newson <geoff.newson@gmail.com>
2020-09-17 23:06:21 +0100

    Working endpoint for the particulate sensor

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
-#from flask import Flask, jsonify
 from flask import Flask
 
 # pimoroni imports
 # particulate sensor
 from pms5003 import PMS5003, ReadTimeoutError
 import logging
+########
 
+application = Flask(__name__)
+
+######
 logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
     level=logging.INFO,
@@ -14,24 +17,38 @@ logging.basicConfig(
 logging.info("app started")
 
 pms5003 = PMS5003()
-
 #######
-
-application = Flask(__name__)
 
 @application.route("/particulates")
 def particulates():
-    #try:
-        #readings = pms5003.read()
-        #logging.info(readings)
-    logging.info("message received")
-    #except ReadTimeoutError:
-        #pms5003 = PMS5003()
+    global pms5003
 
-  #return jsonify({"ButtonPress":"Success"})
-    #return readings
-    return "hello world", 200
+    try:
+        readings = pms5003.read()
+        logging.info(readings)
+    except ReadTimeoutError:
+        # TODO probably don't need to do this as it's from the example loop
+        # should definitely print out the error though
+        # QUESTION: should we handle the other possible errors
+        pms5003 = PMS5003()
+
+    detailsToReturn = {"PM1.0":readings.pm_ug_per_m3(1.0),
+                       "PM2.5":readings.pm_ug_per_m3(2.5),
+                       "PM10":readings.pm_ug_per_m3(10),
+                       "PM1.0a":readings.pm_ug_per_m3(1.0, True),
+                       "PM2.5a":readings.pm_ug_per_m3(2.5, True),
+                       "PM10a":readings.pm_ug_per_m3(None, True),
+                       ">0.3":readings.pm_per_1l_air(0.3),
+                       ">0.5":readings.pm_per_1l_air(0.5),
+                       ">1.0":readings.pm_per_1l_air(1.0),
+                       ">2.5":readings.pm_per_1l_air(2.5),
+                       ">5.0":readings.pm_per_1l_air(5.0),
+                       ">10.0":readings.pm_per_1l_air(10.0)}
+
+    # QUESTION: do we want to use jsonify?
+    return detailsToReturn
 
 if __name__ == "__main__":
   application.run(debug=True, host='0.0.0.0')
+
 
