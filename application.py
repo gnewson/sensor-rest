5ef87b383da9a8974837218ffb6948e65a49d896
Geoff Newson <geoff.newson@gmail.com>
2020-09-17 21:25:49 +0100

    testing some changes

--- application.py
+++ application.py
@@ -3,13 +3,15 @@ from flask import Flask
 
 # pimoroni imports
 # particulate sensor
-import pms5003 import PMS5003, ReadTimeoutError
+from pms5003 import PMS5003, ReadTimeoutError
 import logging
 
 logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.info("app started")
 
 pms5003 = PMS5003()
 
@@ -19,14 +21,17 @@ application = Flask(__name__)
 
 @application.route("/particulates")
 def particulates():
-    try:
-        readings = pms5003.read()
-        logging.info(readings)
-    except ReadTimeoutError:
-        pms5003 = PMS5003()
+    #try:
+        #readings = pms5003.read()
+        #logging.info(readings)
+    logging.info("message received")
+    #except ReadTimeoutError:
+        #pms5003 = PMS5003()
 
   #return jsonify({"ButtonPress":"Success"})
-  return readings
+    #return readings
+    return "hello world", 200
 
 if __name__ == "__main__":
-  application.run(debug=True)
+  application.run(debug=True, host='0.0.0.0')
+
